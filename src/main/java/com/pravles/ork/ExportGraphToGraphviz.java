fd299190354d7a5a1bea0f8dc607f7040b2894aa
Pravles Redneckoff <me@pravles.com>
2026-06-25 11:17:56 +0200

    Implementing export to Graphviz dot

--- src/main/java/com/pravles/ork/ExportGraphToGraphviz.java
+++ src/main/java/com/pravles/ork/ExportGraphToGraphviz.java
@@ -123,7 +123,7 @@ public class ExportGraphToGraphviz implements com.pravles.processengine.api.Acti
             sb.append(WordUtils.wrap(title, TITLE_WIDTH, "<br/>", true));
         }
 
-        return format("  \"%s\" [label=\"%s\", shape=ellipse, width=%.2f, height=%.2f]%s",
+        return format("  \"%s\" [label=<%s>, shape=ellipse, width=%.2f, height=%.2f]%s",
                 nodeId, sb.toString(), 1.5*size, 1.0*size, NL);
     }
 
